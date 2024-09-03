787e396ac2e6943fcc34a5e2ffb9da4a3a441ba6
PamelaGA18 <bticiagallegos@gmail.com>
2024-09-02 20:04:37 -0600

    Proyecto Lenguajes y Automatas 2

---
+++ src/main/java/com/tar/comp_la2gpa_gallegos/Comp_La2gpA_gallegos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tar.comp_la2gpa_gallegos;
+
+/**
+ *
+ * @author btici
+ */
+public class Comp_La2gpA_gallegos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
